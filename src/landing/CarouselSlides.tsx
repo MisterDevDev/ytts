c27df3a27228452be28e3839e94e6d1c4ff01e3e
suncom-devin-boyd <devin.boyd@dms.fl.gov>
2022-07-30 20:49:39 -0400

    fixed carousel size

--- src/landing/CarouselSlides.tsx
+++ src/landing/CarouselSlides.tsx
@@ -3,20 +3,23 @@ import { Carousel, Row, Col, Container } from "react-bootstrap";
 
 const CarouselSlides = () => {
   return (
-    <Container>
-      <Row className="mt-5">
-        <Col className="mt-3">
-        <div style={{padding:'1rem'}}>
+    <Row className="mt-5">
+      <Col style={{ padding: "2rem" }} className="mt-3">
+        <div>
           <h2>Youth Excellence</h2>
           <p>
-            At YTTS we believe in instilling good habits and strong community in junior athletes to condition long time success.
-            Not only does Yukako take great care to improve the game of all her athletes; she also handles the tedious details
-            involved with travel tournaments so athletes and their family can focus on the important things. With five years of
-            experience in coaching and an uncompromising work ethic, Coach Yukako will is driven to help young athletes to achieve.
+            At YTTS we believe in instilling good habits and strong community in
+            junior athletes to condition long time success. Not only does Yukako
+            take great care to improve the game of all her athletes; she also
+            handles the tedious details involved with travel tournaments so
+            athletes and their family can focus on the important things. With
+            five years of experience in coaching and an uncompromising work
+            ethic, Coach Yukako will is driven to help young athletes to
+            achieve.
           </p>
         </div>
       </Col>
-      <Col md={{offset:1}}>
+      <Col>
         <Carousel
           interval={5000}
           controls={true}
@@ -53,9 +56,8 @@ const CarouselSlides = () => {
           </Carousel.Item>
         </Carousel>
       </Col>
-      </Row> 
-    </Container>
+    </Row>
   );
 };
 
-export default CarouselSlides
+export default CarouselSlides;
