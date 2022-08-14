1ebd4e6a7fe9e7aef4276c179c8ef1d39c109a65
suncom-devin-boyd <devin.boyd@dms.fl.gov>
2022-08-14 18:02:57 -0400

    fixing scrolling

--- src/landing/CoachVideo.tsx
+++ src/landing/CoachVideo.tsx
@@ -5,29 +5,41 @@ const CoachVideo = () => {
   return (
     <Container>
       <section id="section-coach" className="mt-5">
-        <Row style={{paddingTop:'4rem'}}>
-          <Col>
-            <img style={{marginLeft: 'auto', marginRight:'auto', display:'block'}} 
-            height={'650px'} width={'350px'}
-            src='https://ytts-coaching.s3.us-east-2.amazonaws.com/IMG_3606.jpg'/>
+        <Row style={{ paddingTop: "4rem" }}>
+          <Col xs={12} md={6}>
+            <img
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+              height={"650px"}
+              width={"350px"}
+              src="https://ytts-coaching.s3.us-east-2.amazonaws.com/IMG_3606.jpg"
+            />
           </Col>
-          <Col>
-          <Card>
-            <Card.Header style={{backgroundColor:'#539e8a'}}><h2>Coach Yukako Noi</h2></Card.Header>
-            <Card.Body>
-              <div style={{padding:'1rem'}}>
-                <h2 className="display-4"></h2>
-                <p>Graduated Florida State University {'(2017)'}</p>
-                <p>&emsp;&emsp;-Sports Management - Major</p>
-                <p>&emsp;&emsp;-Business - Minor</p>
-                <p>Tennis coach at IMG academy {'(2017-2021)'}</p>
-                <p>Tour coach for high performance junior players {'(2021-now)'}</p>
-              </div>
-            </Card.Body>
-          </Card>
+          <Col xs={12} md={6}>
+            <Card>
+              <Card.Header style={{ backgroundColor: "#539e8a" }}>
+                <h2>Coach Yukako Noi</h2>
+              </Card.Header>
+              <Card.Body>
+                <div style={{ padding: "1rem" }}>
+                  <h2 className="display-4"></h2>
+                  <p>Graduated Florida State University {"(2017)"}</p>
+                  <p>&emsp;&emsp;-Sports Management - Major</p>
+                  <p>&emsp;&emsp;-Business - Minor</p>
+                  <p>Tennis coach at IMG academy {"(2017-2021)"}</p>
+                  <p>
+                    Tour coach for high performance junior players{" "}
+                    {"(2021-now)"}
+                  </p>
+                </div>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
-        <div style={{marginTop:'2rem'}} className="ratio ratio-16x9">
+        <div style={{ marginTop: "2rem" }} className="ratio ratio-16x9">
           <iframe
             width="885"
             height="498"
